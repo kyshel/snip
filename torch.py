bcfd07b94fab2d1a1bf7895ceb75268e9baff540
kyshel <11898075+kyshel@users.noreply.github.com>
2021-07-31 23:30:43 +0800

    Update torch.py

--- torch.py
+++ torch.py
@@ -1,32 +1,34 @@
  
 
 # calc dataset mean&std 
-# ref https://discuss.pytorch.org/t/about-normalization-using-pre-trained-vgg16-networks/23560/39
+# ref1 https://discuss.pytorch.org/t/about-normalization-using-pre-trained-vgg16-networks/23560/39
+# ref2 https://stackoverflow.com/a/60803379
 
-dataloader = torch.utils.data.DataLoader(raw_train, batch_size=1, num_workers=1, shuffle=False)
- 
-mean = torch.zeros(3)
-std = torch.zeros(3)
+transform = transforms.Compose([transforms.ToTensor(),])
+dataset = Emoji(root='./data', train=True,
+                    transform=transform)
+loader = torch.utils.data.DataLoader(dataset, batch_size=1)
 
-for i, data in enumerate(dataloader):
-    if (i % 10000 == 0): print(i)
-    data = data[0].squeeze(0)
-    if (i == 0): size = data.size(1) * data.size(2)
-    mean += data.sum((1, 2)) / size
 
-mean /= len(dataloader)
+nimages = 0
+mean = 0.0
+var = 0.0
+for i_batch, batch_target in enumerate(loader):
+    batch = batch_target[0]
+    # Rearrange batch to be the shape of [B, C, W * H]
+    batch = batch.view(batch.size(0), batch.size(1), -1)
+    # Update total number of images
+    nimages += batch.size(0)
+    # Compute mean and std here
+    mean += batch.mean(2).sum(0) 
+    var += batch.var(2).sum(0)
+
+mean /= nimages
+var /= nimages
+std = torch.sqrt(var)
+
 print(mean)
-mean = mean.unsqueeze(1).unsqueeze(2)
-
-for i, data in enumerate(dataloader):
-    if (i % 10000 == 0): print(i)
-    data = data[0].squeeze(0)
-    std += ((data - mean) ** 2).sum((1, 2)) / size
-
-std /= len(dataloader)
-std = std.sqrt()
 print(std)
-
 
 
 # below chunks same as torch.arange(60).view(3,4,5)
